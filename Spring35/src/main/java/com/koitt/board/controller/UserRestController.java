ada17b315481c0112f02508c21a377beed3f668d
Geok0de <gns25107@naver.com>
2017-12-07 20:04:48 +0900

    Spring35 Client

--- Spring35/src/main/java/com/koitt/board/controller/UserRestController.java
+++ Spring35/src/main/java/com/koitt/board/controller/UserRestController.java
@@ -1,5 +1,7 @@
 package com.koitt.board.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,30 +15,38 @@ import com.koitt.board.service.UserInfoService;
 
 public class UserRestController {
 
+	private Logger logger = LogManager.getLogger(this.getClass());
+
 	@Autowired
 	private UserInfoService userInfoService;
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
+
 	// /user/login POST REST API 추가하기...
 	@RequestMapping(value = "/user/login", method = RequestMethod.POST)
-	  public ResponseEntity<String> login(UserInfo userInfo) {
+	public ResponseEntity<String> login(UserInfo userInfo) {
+
+		logger.debug(userInfo);
+
 		// 아이디 존재 유무와 비밀번호 일치 여부 확인
 		boolean isMatched = userInfoService.isPasswordMatched(userInfo.getEmail(), userInfo.getPassword());
-		
-		if(isMatched) {
-			
+
+		if (isMatched) {
+
 			// Base64 인코딩 전 평문
 			String plainCredentials = userInfo.getEmail() + ":" + userInfo.getPassword();
-			
+
 			// 평문을 Base64로 인코딩
 			String base64Credentials = new String(Base64.encodeBase64(plainCredentials.getBytes()));
-			
+
+			logger.debug(base64Credentials);
+
 			return new ResponseEntity<String>(base64Credentials, HttpStatus.OK);
 		}
-		
+
+		logger.debug("login failed");
 		return new ResponseEntity<String>("", HttpStatus.NOT_FOUND);
 	}
-	
+
 }
